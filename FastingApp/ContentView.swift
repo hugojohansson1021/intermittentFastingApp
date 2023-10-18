a9336214265927b992b9698f198457e9903edbe1
hugojohansson <carlhugojohansson@outlook.com>
2023-10-18 03:28:01 +0200

    Färger UI

--- FastingApp/ContentView.swift
+++ FastingApp/ContentView.swift
@@ -27,31 +27,30 @@ struct ContentView: View {
     
     
     var body: some View {
+        
+        
+        
         ZStack{
         //MARK: Background
             
             LinearGradient(gradient: Gradient(colors: [Color.darkPurple, Color.purpleDark, Color.darkPink]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
-            
-            //Color(#colorLiteral(red: 0.07061080267, green: 0.3755476364, blue: 0.4745892397, alpha: 1))
-              //  .ignoresSafeArea()
-            
-            
             content
         }
     }
     
     var content: some View {
         
+        //MARK: scrollview
+        ScrollView{
+            
         VStack (spacing: 40){
             
             //MARK: Title
-            
             Text(title)
                 .font(.headline)
                 
             // MARK: FAsting plan
-             
             Text(fastingManager.fastingPlan.rawValue)
                 .fontWeight(.bold)
                 .padding(.horizontal, 24)
@@ -59,11 +58,10 @@ struct ContentView: View {
                 .background(.thinMaterial)
                 .cornerRadius(20)
             
-            
             //MARK: Progressring
-            
             ProgressRing()
                 .environmentObject(fastingManager)
+            
             
             
             HStack(spacing: 60){
@@ -76,8 +74,6 @@ struct ContentView: View {
                     
                     Text(fastingManager.startTime, format: .dateTime.weekday().hour().minute().second())
                         .fontWeight(.bold)
-                    
-                    
                 }
                 
             //MARK: End Time
@@ -89,16 +85,12 @@ struct ContentView: View {
                     Text(fastingManager.endTime, format: .dateTime.weekday().hour().minute().second())
                         .fontWeight(.bold)
                 }
-                
-                
-                
+            
             }
             
             //MARK: Button
-            
             Button {
                 fastingManager.toggleFastingState()
-            
             } label: {
                 Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
                     .fontWeight(.bold)
@@ -108,21 +100,47 @@ struct ContentView: View {
                     .cornerRadius(20)
                 
             }
-            
-            
-            
-            
-            
             Spacer()
             
             
+            //MARK: Bubbles
+            VStack(spacing: 5){
+                    Rectangle()
+                        .frame(width: 350, height: 150)
+                        .cornerRadius(20.0)
+                }
+            VStack(spacing:1){
+                Rectangle()
+                    .frame(width: 350, height: 150)
+                    .cornerRadius(20.0)
+            }
+                
+            
+            
+            
+          //navigation bar
+          
+           
+            
+            
+            
+            
+            }
           
         }
-       
         .foregroundColor(.white)
+        
+        
+        
     }
+    
+    
+    //
 }
+        
+        
 
 #Preview {
     ContentView()
 }
+
